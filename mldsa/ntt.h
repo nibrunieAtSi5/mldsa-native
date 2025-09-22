22e82e66a347624823a935b90c0dfd354a6acc2a
willieyz <willie.zhao@chelpis.com>
2025-09-22 14:02:07 +0800

    Change MLD_INTT_BOUND to (MLDSA_Q * 3 / 4)
    Thus far we have used 4211139 as the output bound for the inverse NTT which is
    the minimum bound that holds for the C implementation. However, this bound is
    too tight for the AArch64 implementation which may produce slightly larger
    values.
    This commit changes the bound to (MLDSA_Q * 3 / 4) for which it is easy to
    reason that both the AArch64 and AVX2 implementation achieve it.
    The C implementation obviously achieves it.
    
    Signed-off-by: willieyz <willie.zhao@chelpis.com>

--- mldsa/ntt.h
+++ mldsa/ntt.h
@@ -12,7 +12,7 @@
 /* Absolute exclusive upper bound for the output of the forward NTT */
 #define MLD_NTT_BOUND (9 * MLDSA_Q)
 /* Absolute exclusive upper bound for the output of the inverse NTT*/
-#define MLD_INTT_BOUND 4211139
+#define MLD_INTT_BOUND (MLDSA_Q * 3 / 4)
 
 #define mld_ntt MLD_NAMESPACE(ntt)
 /*************************************************
